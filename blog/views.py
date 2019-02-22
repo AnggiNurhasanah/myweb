a8c6693b324938194af4d0cf7fdcdc94211c0d7b
AnggiNurhasanah <AnggiNurhasanah19@gmail.com>
2019-02-22 14:50:36 +0700

    update

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
-
+from blog.models import Artikel
 # Create your views here.
 def index(request):
     return render(request, 'layout/index.html')
+
 def about(request):
     return render(request, 'layout/about.html')
+
 def contact(request):
     return render(request, 'layout/contact.html')
+
 def blog(request):
-    return render(request, 'layout/blog.html')
+    # select * from Artikel 
+    blogs = Artikel.objects.all()
+    return render(request, 'layout/blog.html', {'blogs' : blogs})
